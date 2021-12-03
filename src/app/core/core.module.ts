b05c454a48cef76ce38d409231beb3636fb2717e
olefirenque <egor.olefirenko892@gmail.com>
2021-12-03 12:29:05 +0300

    feat: implement api service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ApiService} from "./services/api.service";
 
 
 @NgModule({
-  declarations: [
-
-  ],
+  declarations: [],
   imports: [
     CommonModule
+  ], providers: [
+    ApiService
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_url: "https://v7j4x1qg63.execute-api.eu-west-1.amazonaws.com",
 };
 
 /*
